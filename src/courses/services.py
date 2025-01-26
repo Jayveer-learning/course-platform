bdca020f8b34dd19f04b2b90868b9cb997737e56
Jay veer <jayveeronpath@gmail.com>
2025-01-27 02:38:13 +0530

    Now we completed views setup. then we going to routing views url. for database look up for objects.

---
+++ src/courses/services.py
@@ -0,0 +1,39 @@
+from .models import (
+    Course,
+    PublishedStatus,
+    Lesson
+)
+
+
+def get_publish_course():
+    return Course.objects.filter(status=PublishedStatus.PUBLISHED) # return objects if status field of course is Published so we use PublishedStatus,PUBLISHED class for filtering the Course objects. 
+
+def get_course_detail(course_id=None):
+    if course_id is None:
+        return None
+    obj = None
+    try:
+        obj = Course.objects.get(
+            status=PublishedStatus.PUBLISHED,
+            id=course_id
+        ) # so get data from Course objects that have status is published and id is equal to course_id is both value match then get method get the object from Course models database. and then return obj.  
+    except:
+        pass 
+    return obj
+
+
+def get_lesson_detail(course_id=None, lesson_id=None):
+    if lesson_id is None or course_id is None:
+        return None
+    obj = None
+    try:
+        obj = Lesson.objects.get(
+            course__id=course_id, # double underscore is use to travel foreign key relationship in django orm. means you can access from lesson course field. 
+            course__status=PublishedStatus.PUBLISHED, # double underscore use to travel model relationship and access. 
+            status=PublishedStatus.PUBLISHED,
+            id=lesson_id
+        ) # in this if course__id=course_id. status of lesson course if is public course__status=PublishedStatus.PUBLISHED and id of lesson model if equal to that we provide id=lesson_id if all of this data match then get method get then data of lesson object return. 
+    except:
+        pass
+    return obj
+
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,30 @@
+from django.http import Http404
 from django.shortcuts import render, HttpResponse
 
+from . import services
+
 # Create your views here.
+def course_list_view(request):
+    queryset = services.get_publish_course()
+    return render(request, "courses/list.html", {})
+
+'''
+path("course_detail/<int:id>/", views.course_detail_views, name="Course detail")    
+'''
+def course_detail_view(request, course_id=None, *args, **kwargs):
+    course_obj = services.get_course_detail(course_id=course_id)
+    if course_obj is None:
+        raise Http404()
+    return render(request, "courses/detail.html", {})
+
+'''
+path("lesson_detail/<int:id>/<int:lesson_id>/", views.course_detail_views, name="Course detail")    
+'''
+def lesson_list_view(request, id=None, lesson_id=None, *args, **kwargs):
+    lesson_obj = services.get_lesson_detail(
+        course_id=id, 
+        lesson_id=lesson_id
+    )
+    if lesson_obj is None:
+        raise Http404()
+    return render(request, "courses/lesson.html", {})
